f5cdb53d3eda6e1f8a039a1d39b369c9351caf6c
MiniMestri <fonsild97@hotmail.com>
2023-12-07 19:47:39 +0100

    Desarrollo objeto central
    He desarrollado un objeto simulando que es el planeta

--- Vuelta.py
+++ Vuelta.py
@@ -1,21 +1,37 @@
 import tkinter as tk
 import random
 
+objetos=[]
+numerobjetos=1
 
 class Objetos:
     def __init__(self):
-        self.posx = random.randint(0,1024)
-        self.posy = random.randint(0,1024)
+        self.posx = 512
+        self.posy = 512
         self.radioS = 30
         self.radioM = 300
         self.direccion = random.randint(0,360)
-        self.color = "blue"
+        self.color1 = "blue"
+        self.color2 = "green"
+        self.grosorborde=10
         self.entidad=""
+    def visualizar(self):
+        self.entidad=lienzo.create_oval(
+            self.posx-self.radioM/2,
+            self.posy-self.radioM/2,
+            self.posx+self.radioM/2,
+            self.posy+self.radioM/2,
+            fill=self.color1,
+            outline=self.color2,
+            width=self.grosorborde)
         
 raiz=tk.Tk()
 
 #Creo lienzo
 lienzo=tk.Canvas(width=1024,height=1024)
 lienzo.pack()
+
+objeto=Objetos()
+objeto.visualizar()
 
 raiz.mainloop()

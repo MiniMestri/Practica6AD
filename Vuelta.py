1d95402bbb8a72ba56e806cb1ed97ea47ed0bae4
MiniMestri <fonsild97@hotmail.com>
2023-12-07 19:37:18 +0100

    Constructor

--- Vuelta.py
+++ Vuelta.py
@@ -1,7 +1,20 @@
 import tkinter as tk
+import random
 
+
+class Objetos:
+    def __init__(self):
+        self.posx = random.randint(0,1024)
+        self.posy = random.randint(0,1024)
+        self.radioS = 30
+        self.radioM = 300
+        self.direccion = random.randint(0,360)
+        self.color = "blue"
+        self.entidad=""
+        
 raiz=tk.Tk()
 
+#Creo lienzo
 lienzo=tk.Canvas(width=1024,height=1024)
 lienzo.pack()
 
